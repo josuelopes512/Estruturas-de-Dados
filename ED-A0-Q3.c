55af1354502bc32c17d96220ad1d12db0069ef16
Josue Lopes <josue.s.lopes.512@gmail.com>
2020-02-22 18:25:46 -0300

    Create ED-A0-Q3.c

---
+++ ED-A0-Q3.c
@@ -0,0 +1,23 @@
+/*
+Desenvolva um programa C que, baseado no código a seguir, realize as seguintes tarefas:
+int a, *b;
+i. Leia do usuário um valor a ser atribuído à variável a;
+ii. Faça o ponteiro b apontar para a;
+iii. Dobre o valor de a, utilizando o ponteiro b.
+iv. Aloque nova memória para b;
+v. Leia do usuário um valor a ser atribuído à posição de memória apontada por b.
+vi. Mostre os valores de a e b na tela.
+*/
+
+int main(){
+int a, *b;
+b = &a;
+printf("Digite o valor de a: ");
+scanf("%d", &a);
+*b = 2 * a;
+printf("Valor de b: %d\n", *b);
+b = malloc(sizeof(int));
+printf("Digite o valor de b: ");
+scanf("%d", b);
+printf("Valor de a: %d e b: %d", a, *b);
+}
